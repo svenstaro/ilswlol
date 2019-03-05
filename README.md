8c61a23afb991b999b3c5c2815e251ccdc90b3fe
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-03-06 00:13:29 +0100

    Fix acquiring sessions

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Wer weiss?
 
 # How do
-You need to create a `secrets` file on the deploying machine (not the machine you're deploying to!)
+You need to create a `telegram.secret` file on the deploying machine (not the machine you're deploying to!)
 containing these variables:
 
     TG_API_ID=1337
@@ -13,11 +13,10 @@ You can get `TG_API_ID` and `TG_API_HASH` from the Telegram API settings for
 your application. The `TG_PHONE` is your phone number associated with this app.
 Then run this:
 
-    make venv
-    source local_env.sh
-    python acquire_session.py
+    make install
+    poetry run python acquire_session.py
     make run
-    curl 0.0.0.0:5000
+    curl 0.0.0.0:8080
 
 # Deployment
 You can use the ansible scripts in ansible/ to deploy the site. Remember to run
--- acquire_session.py
+++ acquire_session.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 
+import asyncio
 import logging
 import os
 import getpass
+from dotenv import load_dotenv
 from telethon import TelegramClient
 from telethon.errors import SessionPasswordNeededError
 
 logging.basicConfig(level=logging.INFO)
+load_dotenv(verbose=True)
+
+loop = asyncio.get_event_loop()
 
 
-# Initialize Telegram
-logging.info("Connecting to Telegram...")
-client = TelegramClient('telegram_client', os.environ['TG_API_ID'], os.environ['TG_API_HASH'])
-client.connect()
-logging.info("Logging in to Telegram...")
-if not client.is_user_authorized():
-    logging.info("User unauthorized")
-    client.send_code_request(os.environ['TG_PHONE'])
-    code_ok = False
-    while not code_ok:
-        code = input('Enter the auth code: ')
-        try:
-            code_ok = client.sign_in(os.environ['TG_PHONE'], code)
-        except SessionPasswordNeededError:
-            password = getpass('Two step verification enabled. '
-                               'Please enter your password: ')
-            code_ok = client.sign_in(password=password)
-logging.info("Client initialized succesfully")
+async def acquire_session():
+    # Initialize Telegram
+    logging.info("Connecting to Telegram...")
+    client = TelegramClient('telegram_client', os.environ['TG_API_ID'], os.environ['TG_API_HASH'], loop=loop)
+    await client.connect()
+    logging.info("Logging in to Telegram...")
+    if not await client.is_user_authorized():
+        logging.info("User unauthorized")
+        await client.send_code_request(os.environ['TG_PHONE'])
+        code_ok = False
+        while not code_ok:
+            code = input('Enter the auth code: ')
+            try:
+                code_ok = await client.sign_in(os.environ['TG_PHONE'], code)
+            except SessionPasswordNeededError:
+                password = getpass('Two step verification enabled. '
+                                   'Please enter your password: ')
+                code_ok = await client.sign_in(password=password)
+    logging.info("Client initialized succesfully")
+
+loop.run_until_complete(acquire_session())
