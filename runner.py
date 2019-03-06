36a0bfe734d4b44b5db0149bdce6fa4cacada2f9
Sven-Hendrik Haase <svenstaro@gmail.com>
2019-03-06 02:29:46 +0100

    Bind only to localhost

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ from ilswlol.app import app
 
 loop = asyncio.get_event_loop()
 
-server = app.create_server(host="0.0.0.0", port=8080)
+server = app.create_server(host="localhost", port=8080)
 task = asyncio.ensure_future(server)
 loop.run_forever()
 signal(SIGINT, lambda s, f: loop.stop())
